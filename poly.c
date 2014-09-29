81b43d8c28cab0c826fb0cc63d0c0409c58a41ff
fluffytech <atfrontier@hotmail.com.tw>
2014-09-29 15:42:32 +0800

    Add polynomial

---
+++ poly.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    double fx, dfx, x, acoef[200];
+    int n, i;
+    printf("Input rank of the polynomial: ");
+    scanf("%d", &n);
+    for(i=1; i <= n + 1; i++)
+    {
+        printf("#%d coefficient: ", i);
+        scanf("%lf", &acoef[i-1]);
+
+    }
+    printf("Input value of x: ");
+    scanf("%lf", &x);
+    polynomial(x, acoef, n + 1, &fx, &dfx);
+    printf("%lf    %lf", fx, dfx);
+
+
+}
+
+void polynomial(double x, double *a, int np, double *fx, double *dfx)
+{
+    double sum, dsum;
+    int i;
+    for (i = np; i>0; i--)
+    {
+        dsum = dsum * x + sum;
+        sum = sum * x + a[i-1];
+    }
+    *fx = sum;
+    *dfx = dsum;
+}
