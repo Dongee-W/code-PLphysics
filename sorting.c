28d0a50f1be7f45ff55651e4d4e17a072788aa0c
fluffycorp <atfrontier@hotmail.com.tw>
2014-10-08 22:30:55 +0800

    update sorting.c

--- sorting.c
+++ sorting.c
@@ -5,20 +5,27 @@ void sorting(int, int*, int*);
 int main()
 {
     srand(time(NULL));
+    int i, number[NSIZE], numberx[NSIZE], seq[NSIZE];
 
-
-    int i, number[NSIZE], seq[NSIZE];
-
-    // fill the number array
-    for (i=0; i<NSIZE; i++) number[i] = rand() % 256;
-    // fill the seq array
+    // fill the number array and copy to numberx
+    for (i=0; i<NSIZE; i++)
+    {
+        number[i] = rand() % 256;
+        numberx[i] = number[i];
+    }
+    // initialize the seq array
     for (i=0; i<NSIZE; i++) seq[i] = i;
 
-
+    printf("original number array: ");
     printar(number);
-    sorting(NSIZE, number, seq);
-    printar(number);
-    //printar(seq);
+    sorting(NSIZE, numberx, seq);
+    printf("Sorted number array: [");
+    for (i=0; i<NSIZE; i++)
+    {
+        printf("%d", number[seq[i]]);
+        if (i != NSIZE - 1) printf(", ");
+    }
+    printf("]");
 }
 
 // print the array
@@ -37,16 +44,20 @@ void printar(int* a)
 
 void sorting(int size, int* array, int* seqq)
 {
-    int i, j, temp;
+    int i, j, temp1, temp2;
     for (i=0; i<=size-2; i++)
     {
         for (j=i+1; j<=size - 1; j++)
         {
             if (array[i] > array[j])
             {
-                temp = array[i];
+                // swap the order for array and seq
+                temp1 = array[i];
+                temp2 = seqq[i];
                 array[i] = array[j];
-                array[j] = temp;
+                seqq[i] = seqq[j];
+                array[j] = temp1;
+                seqq[j] = temp2;
             }
         }
     }
